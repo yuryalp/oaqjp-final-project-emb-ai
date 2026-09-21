039c9f231ea51d4ba4c59e0d02f4e78e18cdc7bd
yury <yury.al.p@gmail.com>
2026-09-20 20:10:54 -0400

    changes

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -6,22 +6,31 @@ def emotion_detector(text_to_analyze):
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     myobj = { "raw_document": { "text": text_to_analyze } }
     response = requests.post(url, json = myobj, headers=header)
+    emotions_res = {'anger' : 0, 'disgust' : 0, 'fear' : 0, 'joy' : 0, 'sadness' : 0, 'dominant_emotion' : None}
+    if (response.status_code == 400):
+        em_keys = emotions_res.keys()
+        for key in em_keys:
+            emotions_res[key] = None
+        return emotions_res
     response_json = json.loads(response.text)
-    emotions = {}
     for emotion_dic in response_json["emotionPredictions"]:
         emotions = emotion_dic["emotion"]
-        if (response.status_code == 200):
-            em_keys = emotions.keys()
-            for key in em_keys:
-                emotions[key] = None
-            return emotions
-
-        dominant_emotion = ""
         dominant_emotion_score = 0
         for emotion_name, emotion_score in emotions.items():
-            if(emotion_score > dominant_emotion_score):
-                dominant_emotion = emotion_name
+            if (emotion_name == 'anger'):
+                emotions_res['anger'] = emotion_score
+            elif (emotion_name == 'disgust'):
+                emotions_res['disgust'] = emotion_score
+            elif (emotion_name == 'fear'):
+                emotions_res['fear'] = emotion_score
+            elif (emotion_name == 'joy'):
+                emotions_res['joy'] = emotion_score
+            elif (emotion_name == 'sadness'):
+                emotions_res['sadness'] = emotion_score
+            
+            if (emotion_score > dominant_emotion_score):
+                emotions_res["dominant_emotion"] = emotion_name
                 dominant_emotion_score = emotion_score
-        emotions["dominant_emotion"] = dominant_emotion
+        
         break
-    return emotions
+    return emotions_res
--- server.py
+++ server.py
@@ -18,7 +18,10 @@ def em_detector():
     sadness = response["sadness"]
     dominant_emotion = response["dominant_emotion"]
 
-    return f"For the given statement, the system response is 'anger': {anger}, 'disgust': {disgust}, 'fear': {fear}, 'joy': {joy} and 'sadness': {sadness}. The dominant emotion is {dominant_emotion}."
+    if (dominant_emotion is  None):
+        return "Invalid text! Please try again!"
+    else:
+        return f"For the given statement, the system response is 'anger': {anger}, 'disgust': {disgust}, 'fear': {fear}, 'joy': {joy} and 'sadness': {sadness}. The dominant emotion is {dominant_emotion}."
 
 if __name__ == "__main__":
       app.run(host="0.0.0.0", port=5000)
