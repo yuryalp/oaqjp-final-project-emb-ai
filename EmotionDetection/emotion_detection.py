2f8dae3a439a5afa5f74560c5d62ff30dd37ee17
yury <yury.al.p@gmail.com>
2026-09-21 20:26:39 -0400

    Changes

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -1,10 +1,10 @@
 import requests
 import json
 
-def emotion_detector(text_to_analyze):
+def emotion_detector(text_to_analyse):
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
-    myobj = { "raw_document": { "text": text_to_analyze } }
+    myobj = { "raw_document": { "text": text_to_analyse } }
     response = requests.post(url, json = myobj, headers=header)
     ANGER = 'anger'
     DISGUST = 'disgust'
